cddbc70b60ed5d43d5c1b1e5f9af3ca956524d31
iNet <coolguy00032@gmail.com>
2019-11-25 06:54:47 +1300

    1.2.1 - Fixed preview issues

--- src/comp/PreviewPanel.js
+++ src/comp/PreviewPanel.js
@@ -5,10 +5,6 @@ import { applyClassesToHTML } from "../_general";
 export class PreviewPanel extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            frameHeight: 200
-        };
     }
 
     componentDidMount() {
@@ -18,17 +14,7 @@ export class PreviewPanel extends React.Component {
 
     componentDidUpdate(prevProps) {
         // update html in frame
-        var frame = document.getElementById("preview-iframe"),
-            doc = frame.contentDocument || frame.contentWindow.document;
-        
-        doc.body.innerHTML = applyClassesToHTML(this.props.app.state.html, this.props.app.state.css, this.props.app.state.removeComments);
-
-        // determine height
-        if(prevProps !== undefined && (prevProps.app.state.html !== this.props.app.state.html)) {
-            this.setState({
-                frameHeight: doc.body.scrollHeight
-            });
-        }
+        document.getElementById("preview-div").innerHTML = applyClassesToHTML(this.props.app.state.html, this.props.app.state.css, this.props.app.state.removeComments);
     }
 
 
@@ -36,14 +22,8 @@ export class PreviewPanel extends React.Component {
         return (
             <div className={"panel preview " + this.props.app.state.theme}>
                 <ToyhouseProfile app={this.props.app}>
-                    <iframe
-                        title="Profile"
-                        src=""
-                        id="preview-iframe"
-                        style={{
-                            height: this.state.frameHeight + "px"
-                        }}
-                        frameBorder="0"
+                    <div
+                        id="preview-div"
                     />
                 </ToyhouseProfile>
                 
